efd8bb081a555ba360a9847b3ab8312d2c7a3394
Caio Alexandre Troti Caetano <caio_aletroca@hotmail.com>
2024-08-08 13:49:09 -0300

    More scaffolding

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,10 +1,25 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import RootLayout from './pages/root.tsx'
 import './index.css'
+import HomePage from './pages/HomePage/index.tsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout />,
+    children: [
+      {
+        path: '/',
+        element: <HomePage />
+      }
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
